58445874d4faaf56269180b28f39249ec709e928
Melissa Sedlmayr <45108574+wishspinner@users.noreply.github.com>
2019-08-02 15:45:57 -0700

    Restore category read restriction

--- app/actions_controller.rb
+++ app/actions_controller.rb
@@ -11,6 +11,7 @@ class DiscourseUserGallery::ActionsController < ::ApplicationController
     query = ::Upload.joins(posts: {topic: :category}).
       where(posts: {user_id: user.id}).
       where(categories: {id: '7'}).
+      where(categories: {read_restricted: false}).
       where.not(width: '72').
       order('posts.created_at DESC').
       group('uploads.id', 'posts.created_at')
